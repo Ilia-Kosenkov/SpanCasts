7571997122ca5362e124c9c4a675d9df776a1b1f
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-04-10 15:58:15 +0300

    Tests

--- Tests/GetRowTests.cs
+++ Tests/GetRowTests.cs
@@ -85,7 +85,7 @@ namespace Tests
 
             Span2D<ulong> span2D = array.AsSpan2D();
 
-            Span<ulong> row = span2D.GetRowMut(rowId);
+            Span<ulong> row = SpanExtensions.GetRow(span2D, rowId);
 
             for (var i = 0; i < row.Length; i++)
             {
@@ -118,7 +118,7 @@ namespace Tests
 
             ReadOnlySpan2D<ulong> span2D = array.AsSpan2D();
 
-            ReadOnlySpan<ulong> row = span2D.GetRow<ulong>(rowId, 0, 3);
+            ReadOnlySpan<ulong> row = span2D.GetRow(rowId, 0, 3);
 
             for (var i = 0; i < row.Length; i++)
             {
@@ -136,5 +136,46 @@ namespace Tests
             array[rowId, 2] = 30;
             Assert.AreEqual(30, row[2]);
         }
+
+        [Test]
+        public void Test_Span2D_Stack()
+        {
+            Span<long> span1 = stackalloc long[] {1, 2, 3, 4, 5, 6};
+            Span2D<long> span2D = span1.ToSpan2D(2, 3);
+            Span<long> span2 = SpanExtensions.GetRow(span2D, 0);
+            Span<long> span3 = SpanExtensions.GetRow(span2D, 1);
+
+            Assert.IsTrue(span1.Slice(0, 3) == span2);
+            Assert.IsTrue(span1.Slice(3, 3) == span3);
+
+
+            span2[1] = 20;
+            span3[1] = 50;
+            Assert.AreEqual(20, span1[1]);
+            Assert.AreEqual(50, span1[4]);
+            Assert.IsTrue(span1.Slice(0, 3) == span2);
+            Assert.IsTrue(span1.Slice(3, 3) == span3);
+        }
+        
+        [Test]
+        public void Test_ReadOnlySpan2D_Stack()
+        {
+            Span<long> span1 = stackalloc long[] {1, 2, 3, 4, 5, 6};
+            ReadOnlySpan2D<long> span2D = ((ReadOnlySpan<long>)span1).ToReadOnlySpan2D(2, 3);
+            ReadOnlySpan<long> span2 = SpanExtensions.GetRow(span2D, 0);
+            ReadOnlySpan<long> span3 = SpanExtensions.GetRow(span2D, 1);
+
+            Assert.IsTrue(span1.Slice(0, 3) == span2);
+            Assert.IsTrue(span1.Slice(3, 3) == span3);
+
+
+            span1[1] = 20;
+            span1[4] = 50;
+            Assert.AreEqual(20, span2[1]);
+            Assert.AreEqual(50, span3[1]);
+            Assert.IsTrue(span1.Slice(0, 3) == span2);
+            Assert.IsTrue(span1.Slice(3, 3) == span3);
+        }
+        
     }
 }
